a29ba54a163beff7f1f9695305257a6c6ad2ec37
Marcos Gustavo <198700267+Marcosgrdr@users.noreply.github.com>
2025-10-26 11:25:29 -0300

    Initial commit

---
+++ nivis/src/main/java/br/com/royalpair/nivis/NivisApplication.java
@@ -0,0 +1,13 @@
+package br.com.royalpair.nivis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NivisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NivisApplication.class, args);
+	}
+
+}
---
+++ nivis/src/test/java/br/com/royalpair/nivis/NivisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.royalpair.nivis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NivisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
